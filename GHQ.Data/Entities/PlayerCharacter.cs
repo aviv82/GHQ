072d05315a9311be0833f1ef6ca335c3d559c3ba
Aviv <89319835+aviv82@users.noreply.github.com>
2024-06-08 22:41:17 +0200

    Delete GHQ.Data directory

--- GHQ.Data/Entities/PlayerCharacter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GHQ.Data.Entities;
-
-public class PlayerCharacter
-{
-    public int PlayerId { get; set; }
-    public int CharacterId { get; set; }
-    public Player Player { get; set; } = null!;
-    public Character Character { get; set; } = null!;
-}
--- GHQ.Data/EntityServices/Interfaces/IGameService.cs
+++
@@ -1,8 +0,0 @@
-﻿using GHQ.Data.Entities;
-
-namespace GHQ.Data.EntityServices.Interfaces;
-
-public interface IGameService : IBaseService<Game>
-{
-    Task<Game> GetGameByIdIncludingPlayersAndCharacters(int id, CancellationToken cancellationToken);
-}
