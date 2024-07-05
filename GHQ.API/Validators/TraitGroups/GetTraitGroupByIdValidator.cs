dd00b6c0ab5cd3e73dfd824d0065258ce2c3b3a5
Aviv <aviv.dror@gmail.com>
2024-07-06 01:53:08 +0200

    trait group crud sans character

--- GHQ.API/Validators/TraitGroups/GetTraitGroupByIdValidator.cs
+++ GHQ.API/Validators/TraitGroups/GetTraitGroupByIdValidator.cs
@@ -1,16 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FluentValidation;
 using GHQ.Core.TraitGroupLogic.Queries;
+using GHQ.Data.Context.Interfaces;
+using GHQ.Core.Extensions;
+using GHQ.Data.Entities;
 
 namespace GHQ.API.Validators.TraitGroups;
 public class GetTraitGroupByIdValidator : AbstractValidator<GetTraitGroupByIdQuery>
 {
-    public GetCharacterByIdValidator(IGHQContext context)
+    public GetTraitGroupByIdValidator(IGHQContext context)
     {
         RuleFor(x => x.Id)
-        .ValidateExistence<GetTraitGroupByIdQuery, Character>(context)
+        .ValidateExistence<GetTraitGroupByIdQuery, TraitGroup>(context)
         .WithMessage("Trait Group not found.");
     }
 }
