9f603289b0dbfc12972581677f159a3f6b160400
Aviv <aviv.dror@gmail.com>
2025-03-22 22:35:53 +0100

    update roll service

--- GHQ.API/Controllers/RollController.cs
+++ GHQ.API/Controllers/RollController.cs
@@ -75,61 +75,61 @@ public class RollController : Controller
     /// <param name="request">A request object inferred from the query in the URL.</param>
     /// <param name="cancellationToken">A cancellation token.</param>
     /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
-    // [HttpGet("{Id}")]
-    // public async Task<ActionResult<RollDto>> GetById(
-    //     [FromRoute] GetRollByIdQuery request,
-    //     CancellationToken cancellationToken = default)
-    // {
-    //     var validationResult = await _rollByIdValidator.ValidateAsync(request, cancellationToken);
-    //     if (!validationResult.IsValid)
-    //     {
-    //         return BadRequest(validationResult.Errors);
-    //     }
+    [HttpGet("{Id}")]
+    public async Task<ActionResult<RollDto>> GetById(
+        [FromRoute] GetRollByIdQuery request,
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = await _rollByIdValidator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.Errors);
+        }
 
-    //     try
-    //     {
-    //         var result = await _rollHandler.GetRollById(request, cancellationToken);
-    //         return Ok(result);
-    //     }
-    //     catch (BusinessException e)
-    //     {
-    //         return BadRequest(e.Message);
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         _logger.LogError(e, e.Message);
-    //         return new ObjectResult(e.Message) { StatusCode = 500 };
-    //     }
-    // }
+        try
+        {
+            var result = await _rollHandler.GetRollById(request, cancellationToken);
+            return Ok(result);
+        }
+        catch (BusinessException e)
+        {
+            return BadRequest(e.Message);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            return new ObjectResult(e.Message) { StatusCode = 500 };
+        }
+    }
 
-    // /// <summary>
-    // ///  Add Roll.
-    // /// </summary>
-    // /// <param name="request">A request object inferred from the body.</param>
-    // /// <param name="cancellationToken">A cancellation token.</param>
-    // /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
-    // [HttpPost("add")]
-    // public async Task<ActionResult<RollDto>> Add(
-    //     [FromBody] AddRollRequest request,
-    //     CancellationToken cancellationToken = default)
-    // {
-    //     var validationResult = await _addValidator.ValidateAsync(request, cancellationToken);
-    //     if (!validationResult.IsValid)
-    //     {
-    //         return BadRequest(validationResult.Errors);
-    //     }
+    /// <summary>
+    ///  Add Roll.
+    /// </summary>
+    /// <param name="request">A request object inferred from the body.</param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+    [HttpPost("add")]
+    public async Task<ActionResult<RollDto>> Add(
+        [FromBody] AddRollRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = await _addValidator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.Errors);
+        }
 
-    //     try
-    //     {
-    //         var result = await _rollHandler.AddRoll(request, cancellationToken);
-    //         return CreatedAtAction("Add", result);
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         _logger.LogError(e, e.Message);
-    //         return new ObjectResult(e.Message) { StatusCode = 500 };
-    //     }
-    // }
+        try
+        {
+            var result = await _rollHandler.AddRoll(request, cancellationToken);
+            return CreatedAtAction("Add", result);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            return new ObjectResult(e.Message) { StatusCode = 500 };
+        }
+    }
 
     /// <summary>
     ///  Delete Roll.
